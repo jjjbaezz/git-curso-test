8bf1964f810a34d60596e68e2786c08544d9990f
Justin <contact@faztweb.com>
2022-07-04 15:01:16 -0400

    ahora le index html tiene codigo html:)

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("hola mundo");
+
+
+//git ini para agregar un bach al proyecto
+//git add (Nomre del proyecto) para agregarr un proy a tu area de trabajo
+//git status para ver el estado 
+// git config --global user.email "contact@faztweb.com" para asociar los cambios al email del usuario
